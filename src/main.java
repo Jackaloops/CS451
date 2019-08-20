3b50e1c302d0c00ac8f9b0763ef923f9f1747b44
Jack Shen <jack.shen97@gmail.com>
2019-08-20 18:33:08 -0400

    Starting Title Screen

--- src/main.java
+++ src/main.java
@@ -1,5 +1,10 @@
+import view.TitleScreen;
+
 public class main {
     public static void main( String[] args ) {
         System.out.println("Hello World!");
+
+        TitleScreen app = new TitleScreen();
+        app.init();
     }
 }
